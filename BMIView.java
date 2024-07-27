1c1090a5cb979cb553a760afe10bcb53c4bcfccb
Vyvy129H <147341690+Vyvy129H@users.noreply.github.com>
2024-07-27 21:19:55 +0700

    BMI

---
+++ BMIView.java
@@ -0,0 +1,21 @@
+package com.example.demo3;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class BMIView {
+
+    @FXML
+    private TextField Height;
+    @FXML
+    private TextField Weight;
+
+    @FXML
+    protected void checkBMI() {
+        double height = Double.parseDouble(Height.getText());
+        double weight = Double.parseDouble(Weight.getText());
+        double bmi = weight / (height * height);
+        System.out.println("BMI: " + bmi);
+    }
+}
+
